5dc2baf659145f333ae0f60f2618396534c4ded0
noemvy <nvrm726@gmail.com>
2025-05-19 14:23:33 -0500

    Creando: Ruta para probar los datos de la Api OpenWeather

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
-
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/clima', function () {
+    $apiKey = config('services.openweathermap.key');
+
+    $response = Http::get('https://api.openweathermap.org/data/2.5/weather', [
+        'q' => 'Mexico',           // Puedes cambiarlo por cualquier ciudad
+        'appid' => $apiKey,
+        'units' => 'metric',
+        'lang' => 'es',
+    ]);
+
+    return $response->json();
+});
+// Route::get('/clima', function () {
+//     $apiKey = config('services.openweathermap.key');
+
+//     return 'Tu API key es: ' . $apiKey;
+// });
+
