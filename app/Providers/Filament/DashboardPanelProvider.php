6e734708a8a08df2418a561884746cb7106cbaca
noemvy <nvrm726@gmail.com>
2025-05-19 14:21:05 -0500

    Update: Agregando Widgets

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Resources\PatrullajeResource\Widgets\PatrullajeStats;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -32,6 +33,9 @@ class DashboardPanelProvider extends PanelProvider
             ->brandLogoHeight('60px')
             ->colors([
                 'primary' => Color::Lime
+            ])
+            ->widgets([
+                PatrullajeStats::class,
             ])
             ->darkMode(false)
             ->renderHook('panels::body.start', fn()=>'
@@ -79,8 +83,8 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\AccountWidget::class,
+                // Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
