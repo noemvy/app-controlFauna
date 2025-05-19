8f5895ed57766cda1304cf281da1bdbef5082068
noemvy <nvrm726@gmail.com>
2025-05-19 15:16:08 -0500

    Update: Agregando relacion con catalogoInventario

--- app/Models/Acciones.php
+++ app/Models/Acciones.php
@@ -10,4 +10,12 @@ class Acciones extends Model
      protected $table = 'acciones';
 
     protected $fillable = ['nombre'];
+
+
+    // Relación uno a muchos con CatalogoInventario
+    public function catalogoInventario()
+    {
+        return $this->hasMany(CatalogoInventario::class, 'acciones_id');
+        // 'accion_id' es la llave foránea en la tabla catalogo_inventario que referencia a acciones.id
+    }
 }
