a1d282207ecc63a31abf753681f477cb1b330b32
noemvy <nvrm726@gmail.com>
2025-05-22 14:14:09 -0500

    Update: Relacion con intervenciones

---
+++ app/Models/IntervencionesEventoDraft.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+class IntervencionesEventoDraft extends Model
+{
+    use HasFactory;
+    protected $table = 'intervenciones_evento_draft';
+    protected $fillable = [
+        'user_id',
+        'tipo_evento',
+        'origen',
+        'estado',
+        'coordenada_x',
+        'coordenada_y',
+        'temperatura',
+        'viento',
+        'humedad',
+        'especies_id',
+        'atractivos_id',
+        'vistos',
+        'sacrificados',
+        'dispersados',
+        'fotos',
+        'comentarios',
+        'municion_utilizada',
+        'catinventario_id',
+        'acciones_id',
+        'cantidad_utilizada',
+    ];
+    protected $casts = [
+        'coordenada_x' => 'decimal:6',
+        'coordenada_y' => 'decimal:6',
+        'temperatura' => 'float',
+        'viento' => 'float',
+        'humedad' => 'float',
+        'fotos' => 'array',
+        'municion_utilizada' => 'array',
+    ];
+    // Relaciones
+
+        //Relacion con User
+        public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    
+    public function especie()
+    {
+        return $this->belongsTo(Especie::class, 'especies_id');
+    }
+
+    public function catalogoInventario()
+    {
+        return $this->belongsTo(CatalogoInventario::class, 'catinventario_id');
+    }
+
+    public function accion()
+    {
+        return $this->belongsTo(Acciones::class, 'acciones_id');
+    }
+
+    public function atractivo()
+    {
+        return $this->belongsTo(Atractivo::class, 'atractivos_id');
+    }
+
+    public function inventarioMuniciones()
+    {
+        return $this->hasMany(IntervencionesEventoDraft::class);
+    }
+
+    public function evento()
+        {
+            return $this->belongsTo(Evento::class,);
+        }
+
+
+
+
+
+}
+
