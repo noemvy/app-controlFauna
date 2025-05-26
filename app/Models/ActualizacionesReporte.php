c0c1b5c4f0b86b9f860fbe79f322d5a0df370759
noemvy <nvrm726@gmail.com>
2025-05-26 13:19:17 -0500

    Create: Creando el Modelo para la actualizacion de reporte

---
+++ app/Models/ActualizacionesReporte.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class ActualizacionesReporte extends Model
+{
+    use HasFactory;
+    protected $table = 'actualizacion_reportes';
+
+    protected $fillable = ['actualizacion', 'autor'];
+
+    public function reportable()
+    {
+        return $this->morphTo();
+    }
+}
+
