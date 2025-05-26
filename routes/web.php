d4d8f674be555fe7acf83e4332cbc57f2ca56bdf
noemvy <nvrm726@gmail.com>
2025-05-26 13:13:51 -0500

    Add: Ruta para descargar el archivo pdf

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,13 @@
 <?php
+
+use App\Http\Controllers\PDFController;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/report/pdf/{id}', [PDFController::class, 'generatePDF'])->name('report.pdf');
 
-Route::get('/clima', function () {
-    $apiKey = config('services.openweathermap.key');
 
-    $response = Http::get('https://api.openweathermap.org/data/2.5/weather', [
-        'q' => 'Mexico',           // Puedes cambiarlo por cualquier ciudad
-        'appid' => $apiKey,
-        'units' => 'metric',
-        'lang' => 'es',
-    ]);
-
-    return $response->json();
-});
-// Route::get('/clima', function () {
-//     $apiKey = config('services.openweathermap.key');
-
-//     return 'Tu API key es: ' . $apiKey;
-// });
 
