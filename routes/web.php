037e56a584105232ff48752231885f38e5a2a2dd
noemvy <nvrm726@gmail.com>
2025-05-27 15:07:00 -0500

    Update: Agregando ruta para descargar pdfs

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Http\Controllers\PDFController;
+use App\Http\Controllers\PDFIntervencionesEvento;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+//Ruta para el reporte de Impacto con Fauna
 Route::get('/report/pdf/{id}', [PDFController::class, 'generatePDF'])->name('report.pdf');
+
+//Ruta para el reporte de evento intervenciones
+Route::get('/eventoIntervenciones/pdf/{id}', [PDFIntervencionesEvento::class, 'generatePDF'])->name('eventoIntervenciones.pdf');
 
 
 
