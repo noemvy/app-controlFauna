87e272174938f454dd07c39e3260ea0a99af7faa
noemvy <nvrm726@gmail.com>
2025-06-02 17:39:58 -0500

    Update: Cambiando de modelos de intervenciones a los modelos de pivote ya que ahi es donde se encuentran las municiones contabilizadas

--- app/Filament/Widgets/MunicionesChart.php
+++ app/Filament/Widgets/MunicionesChart.php
@@ -1,70 +1,71 @@
 <?php
+namespace App\Filament\Widgets;
 
-// namespace App\Filament\Widgets;
+use App\Models\CatalogoInventario;
+use App\Models\Intervenciones;
+use App\Models\IntervencionesEventoDraft;
+use App\Models\PivoteEvento;
+use App\Models\PivotePatrullajeIntervencion;
+use Filament\Widgets\ChartWidget;
+use Illuminate\Support\Facades\DB;
+class MunicionesChart extends ChartWidget
+{
+    protected static ?string $heading = 'Municiones Usadas';
 
-// use App\Models\CatalogoInventario;
-// use App\Models\Intervenciones;
-// use App\Models\IntervencionesEventoDraft;
-// use Filament\Widgets\ChartWidget;
-// use Illuminate\Support\Facades\DB;
-// class MunicionesChart extends ChartWidget
-// {
-//     protected static ?string $heading = 'Municiones Usadas';
+    protected function getData(): array
+    {
+        //Consultas para obtener las municiones mas usadas en las intervenciones
+        $query1 = PivotePatrullajeIntervencion::select('catinventario_id');
+        $query2 = PivoteEvento::select('catinventario_id');
 
-//     protected function getData(): array
-//     {
-//         //Consultas para obtener las municiones mas usadas en las intervenciones
-//         $query1 = IntervencionesEventoDraft::select('catinventario_id');
-//         $query2 = Intervenciones::select('catinventario_id');
+        $union = $query1->unionAll($query2);
 
-//         $union = $query1->unionAll($query2);
+        //Contar todas las municones que se usan
+        $resultados = DB::table(DB::raw("({$union->toSql()}) as all_municiones"))
+        ->mergeBindings($union->getQuery())
+        ->select('catinventario_id',DB::raw('count(*)as total'))
+        ->groupBy('catinventario_id')
+        ->get()
+        ->map(function($item){
+        $nombre = CatalogoInventario::find($item->catinventario_id)?->nombre ?? 'Sin Uso';
+        return [
+            'nombre'=> $nombre,
+            'total'=>$item->total,
+        ];
+        });
 
-//         //Contar todas las municones que se usan
-//         $resultados = DB::table(DB::raw("({$union->toSql()}) as all_municiones"))
-//         ->mergeBindings($union->getQuery())
-//         ->select('catinventario_id',DB::raw('count(*)as total'))
-//         ->groupBy('catinventario_id')
-//         ->get()
-//         ->map(function($item){
-//         $nombre = CatalogoInventario::find($item->catinventario_id)?->nombre ?? 'Sin Uso';
-//         return [
-//             'nombre'=> $nombre,
-//             'total'=>$item->total,
-//         ];
-//         });
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Municiones',
+                    'data' => $resultados->pluck('total'),
+                    //COLORES PARA LAS ESTADISTICAS
+                    'backgroundColor' => [
+                    '#FF6384', // rojo
+                    '#36A2EB', // azul
+                    '#FFCE56', // amarillo
+                    '#4BC0C0', // turquesa
+                    '#9966FF', // morado
+                    '#FF9F40', // naranja
+                    '#C9CBCF', // gris
+            ],
+                    'borderColor' => [
+                    '#FF6384', // rojo
+                    '#36A2EB', // azul
+                    '#FFCE56', // amarillo
+                    '#4BC0C0', // turquesa
+                    '#9966FF', // morado
+                    '#FF9F40', // naranja
+                    '#C9CBCF', // gris
+            ],
+                ],
+            ],
+            'labels' => $resultados->pluck('nombre'),
+        ];
+    }
 
-//         return [
-//             'datasets' => [
-//                 [
-//                     'label' => 'Municiones',
-//                     'data' => $resultados->pluck('total'),
-//                     //COLORES PARA LAS ESTADISTICAS
-//                     'backgroundColor' => [
-//                     '#FF6384', // rojo
-//                     '#36A2EB', // azul
-//                     '#FFCE56', // amarillo
-//                     '#4BC0C0', // turquesa
-//                     '#9966FF', // morado
-//                     '#FF9F40', // naranja
-//                     '#C9CBCF', // gris
-//             ],
-//                     'borderColor' => [
-//                     '#FF6384', // rojo
-//                     '#36A2EB', // azul
-//                     '#FFCE56', // amarillo
-//                     '#4BC0C0', // turquesa
-//                     '#9966FF', // morado
-//                     '#FF9F40', // naranja
-//                     '#C9CBCF', // gris
-//             ],
-//                 ],
-//             ],
-//             'labels' => $resultados->pluck('nombre'),
-//         ];
-//     }
-
-//     protected function getType(): string
-//     {
-//         return 'doughnut';
-//     }
-// }
+    protected function getType(): string
+    {
+        return 'doughnut';
+    }
+}
