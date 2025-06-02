3cef1b8230b99136c8992fbdf6928eeaaa0242b5
noemvy <nvrm726@gmail.com>
2025-06-02 17:38:31 -0500

    Update: Agregando timestamps

--- database/migrations/2025_06_01_021535_create_pivote_patrullaje_intervenciones_table.php
+++ database/migrations/2025_06_01_021535_create_pivote_patrullaje_intervenciones_table.php
@@ -9,13 +9,14 @@ return new class extends Migration
     {
         Schema::create('pivote_patrullaje_intervenciones', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('catinventario_id')->constrained('catalogo_inventarios')->onDelete('cascade');
+            $table->foreignId('acciones_id')->constrained('acciones')->onDelete('cascade');
+            $table->integer('cantidad_utilizada')->nullable()->default(0);
+            $table->foreignId('intervencion_draft_id')->nullable()->constrained('intervenciones_drafts')->onDelete('cascade');
+            $table->foreignId('intervencion_id')->nullable()->constrained('intervenciones')->onDelete('cascade');
+            $table->timestamps();
 
-            $table->foreignId('intervencion_draft_id')
-                ->nullable()
-                ->constrained('intervenciones_drafts')
-                ->onDelete('cascade');
 
-            
         });
     }
 
