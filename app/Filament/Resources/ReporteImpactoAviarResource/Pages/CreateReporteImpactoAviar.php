6ef35f831fc999d2220d3abbbd07b6e76998a27f
noemvy <nvrm726@gmail.com>
2025-06-02 16:53:16 -0500

    Chore: sin cambios

--- app/Filament/Resources/ReporteImpactoAviarResource/Pages/CreateReporteImpactoAviar.php
+++ app/Filament/Resources/ReporteImpactoAviarResource/Pages/CreateReporteImpactoAviar.php
@@ -5,10 +5,12 @@ namespace App\Filament\Resources\ReporteImpactoAviarResource\Pages;
 use App\Filament\Resources\ReporteImpactoAviarResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Facades\Filament;
 
 class CreateReporteImpactoAviar extends CreateRecord
 {
     protected static string $resource = ReporteImpactoAviarResource::class;
+
     function getRedirectUrl(): string
     {
         return route('filament.dashboard.resources.reporte-impacto-aviars.index');
