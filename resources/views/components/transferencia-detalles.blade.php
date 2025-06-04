d8c9be72134f1134f84ad328a445449b27bd970d
noemvy <nvrm726@gmail.com>
2025-06-04 15:25:43 -0500

    Create: Vista para ver los detalles de transferencia

---
+++ resources/views/components/transferencia-detalles.blade.php
@@ -0,0 +1,26 @@
+<div class="space-y-4 text-sm">
+    <div>
+        <strong>Aeródromo Origen:</strong>
+        <div>{{ $record->transferenciaMuniciones->aerodromoOrigen->nombre ?? 'N/A' }}</div>
+    </div>
+    <div>
+        <strong>Aeródromo Destino:</strong>
+        <div>{{ $record->transferenciaMuniciones->aerodromoDestino->nombre?? 'N/A' }}</div>
+    </div>
+    <div>
+        <strong>Cantidad Transferida:</strong>
+        <div>{{ $record->transferenciaMuniciones->cantidad ?? 'N/A' }}</div>
+    </div>
+    <div>
+        <strong>Fecha y Hora:</strong>
+        <div>{{ $record->transferenciaMuniciones->updated_at }}</div>
+    </div>
+    <div>
+        <strong>Usuario:</strong>
+        <div>{{ $record->user->name ?? 'Sin Usuario' }}</div>
+    </div>
+    <div>
+        <strong>Comentarios:</strong>
+        <div>{{ $record->observaciones ?? 'Sin comentarios' }}</div>
+    </div>
+</div>
