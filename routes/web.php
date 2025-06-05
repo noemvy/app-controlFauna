a15c14ce23721622b7c8bb980401e552771cd0bb
noemvy <nvrm726@gmail.com>
2025-06-05 15:16:30 -0500

    Update: Agregando rutas para los excel de reportes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\EfectividadController;
+use App\Http\Controllers\EspeciesStatsController;
+use App\Http\Controllers\MunicionesStatsController;
 use App\Http\Controllers\PDFController;
 use App\Http\Controllers\PDFIntervencionesEvento;
 use Illuminate\Support\Facades\Http;
@@ -12,7 +15,17 @@ Route::get('/', function () {
 Route::get('/report/pdf/{id}', [PDFController::class, 'generatePDF'])->name('report.pdf');
 
 //Ruta para el reporte de evento intervenciones
-Route::get('/eventoIntervenciones/pdf/{id}', [PDFIntervencionesEvento::class, 'generatePDF'])->name('eventoIntervenciones.pdf');
+Route::get('/eventoIntervenciones/pdf/{id}', [PDFIntervencionesEvento::class, 'generatePDF'])
+->name('eventoIntervenciones.pdf');
 
 
+Route::get('/estadisticas-especies/export-excel', [EspeciesStatsController::class, 'exportExcel'])
+->name('estadisticas.especies.export-excel');
 
+
+Route::get('/estadisticas-municiones/export-excel', [MunicionesStatsController::class, 'exportExcel'])
+->name('estadisticas.municiones.export-excel');
+
+
+Route::get('/estadisticas-efectividad/export-excel', [EfectividadController::class, 'exportExcel'])
+->name('estadisticas.efectividad.export-excel');
