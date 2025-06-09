29c7801ad06e1d93d099d78f9593044c328fdb67
noemvy <nvrm726@gmail.com>
2025-06-09 11:15:44 -0500

    Chore: Sin Cambios

--- app/Filament/Resources/CatalogoInventarioResource.php
+++ app/Filament/Resources/CatalogoInventarioResource.php
@@ -26,83 +26,74 @@ class CatalogoInventarioResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-            ->schema([
-                Forms\Components\Select::make('acciones_id')
-                ->label('Acción')
-                ->options(Acciones::all()->pluck('nombre', 'id'))
-                ->searchable()
-                ->required(),
-
-                Forms\Components\TextInput::make('nombre')
-                ->label('Nombre del equipo')
-                ->placeholder('Escriba el nombre del equipo')
-                ->required()
-                ->maxLength(255),
-
-
-                Forms\Components\Select::make('categoria_equipo')
-                    ->label('Categoria Equipo')
-                    ->options([
-                        'Armas' => 'Armas',
-                        'Municiones' => 'Municiones',
-                        'Instrumento' => 'Instrumento',
-                    ])
-                    ->required(),
-
-                Forms\Components\Select::make('es_consumible')
-                    ->label('Tipo de Equipo')
-                    ->options([
-                        0 => 'No Consumible',
-                        1 => 'Consumible',
-                    ])
-                    ->default(1)
-                    ->required(),
-                Forms\Components\TextInput::make('descripcion')
-                    ->label('Descripción del equipo')
-                    ->maxLength(200),
-                Forms\Components\Select::make('estado')
-                    ->label('Estado')
-                    ->options([
-                        1 => 'Activo',
-                        0 => 'Inactivo',
-                    ])
-                    ->default(1) // Valor por defecto establecido como "Activo"
-                    ->required(),
+    return $form
+    ->schema([
+    Forms\Components\Select::make('acciones_id')
+        ->label('Acción')
+        ->options(Acciones::all()->pluck('nombre', 'id'))
+        ->searchable()
+        ->required(),
+    Forms\Components\TextInput::make('nombre')
+        ->label('Nombre del equipo')
+        ->placeholder('Escriba el nombre del equipo')
+        ->required()
+        ->maxLength(255),
+    Forms\Components\Select::make('categoria_equipo')
+        ->label('Categoria Equipo')
+        ->options([
+            'Armas' => 'Armas',
+            'Municiones' => 'Municiones',
+            'Instrumento' => 'Instrumento',
+        ])
+        ->required(),
+    Forms\Components\Select::make('es_consumible')
+        ->label('Tipo de Equipo')
+        ->options([
+            0 => 'No Consumible',
+            1 => 'Consumible',
+        ])
+        ->default(1)
+        ->required(),
+    Forms\Components\TextInput::make('descripcion')
+        ->label('Descripción del equipo')
+        ->maxLength(200),
+    Forms\Components\Select::make('estado')
+        ->label('Estado')
+        ->options([
+            1 => 'Activo',
+            0 => 'Inactivo',
+        ])
+        ->default(1) // Valor por defecto establecido como "Activo"
+        ->required(),
             ]);
-
     }
 
     public static function table(Table $table): Table
     {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('nombre')->label('Nombre')
-                ->searchable(),
-                Tables\Columns\IconColumn::make('es_consumible')->label('Tipo')
-                ->boolean()
-                    ->trueIcon('heroicon-o-check-circle') // Ícono para verdadero
-                    ->falseIcon('heroicon-o-x-circle') // Ícono para falso
-                    ->trueColor('success') // Color para verdadero
-                    ->falseColor('danger')// Color para falso
-                ->searchable(),
+    return $table
+    ->columns([
+    Tables\Columns\TextColumn::make('nombre')->label('Nombre')
+    ->searchable(),
+    Tables\Columns\IconColumn::make('es_consumible')->label('Consumible')
+    ->boolean()
+        ->trueIcon('heroicon-o-check-circle') // Ícono para verdadero
+        ->falseIcon('heroicon-o-x-circle') // Ícono para falso
+        ->trueColor('success') // Color para verdadero
+        ->falseColor('danger')// Color para falso
+    ->searchable(),
+    Tables\Columns\IconColumn::make('estado')
+        ->label('Activo')
+        ->boolean()
+        ->trueIcon('heroicon-o-check-circle') // Ícono para verdadero
+        ->falseIcon('heroicon-o-x-circle') // Ícono para falso
+        ->trueColor('success') // Color para verdadero
+        ->falseColor('danger'), // Color para falso
+        ])->defaultSort('created_at', 'desc')
+        ->searchable()
 
-                Tables\Columns\IconColumn::make('estado')
-                    ->label('Activo')
-                    ->boolean()
-                    ->trueIcon('heroicon-o-check-circle') // Ícono para verdadero
-                    ->falseIcon('heroicon-o-x-circle') // Ícono para falso
-                    ->trueColor('success') // Color para verdadero
-                    ->falseColor('danger'), // Color para falso
-            ])->defaultSort('created_at', 'desc')
-            ->searchable()
-
-            ->filters([
+    ->filters([
                 //
-            ])
-            ->actions([
-                Tables\Actions\ViewAction::make(),
-            ]);
+    ]);
     }
 
     public static function getRelations(): array
