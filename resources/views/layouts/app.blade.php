3ed6e0f72191ec7045d881b53f6034af59b181be
noemvy <nvrm726@gmail.com>
2025-06-13 14:06:59 -0500

    Create: Archivo layout para usarlo en la vista blade detalles-patrullajes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Control de Fauna</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-50 text-gray-900 font-sans antialiased">
+
+    <div class="max-w-7xl mx-auto p-6">
+        @yield('content')
+    </div>
+
+</body>
+</html>
